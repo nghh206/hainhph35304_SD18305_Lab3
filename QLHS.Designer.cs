5681bd7228cb616e9ee571730b31427abd856cde
Acer <Acer@NGHH>
2023-07-15 07:51:49 +0700

    Add project files.

---
+++ QLHS.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace hainhph35304_SD18305_Lab3
+{
+    partial class QLHS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.cbLop = new System.Windows.Forms.ComboBox();
+            this.txtDiemTB = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtTenHS = new System.Windows.Forms.TextBox();
+            this.txtMaHS = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnNhapLai = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(322, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH SÁCH HỌC SINH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(68, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã học sinh:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(68, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tên học sinh:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpNgaySinh);
+            this.groupBox1.Controls.Add(this.cbLop);
+            this.groupBox1.Controls.Add(this.txtDiemTB);
+            this.groupBox1.Controls.Add(this.txtDiaChi);
+            this.groupBox1.Controls.Add(this.txtTenHS);
+            this.groupBox1.Controls.Add(this.txtMaHS);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Location = new System.Drawing.Point(47, 92);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(717, 472);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin học sinh";
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgaySinh.Location = new System.Drawing.Point(227, 211);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(387, 27);
+            this.dtpNgaySinh.TabIndex = 13;
+            this.dtpNgaySinh.Value = new System.DateTime(2023, 7, 13, 0, 0, 0, 0);
+            this.dtpNgaySinh.ValueChanged += new System.EventHandler(this.dtpNgaySinh_ValueChanged);
+            // 
+            // cbLop
+            // 
+            this.cbLop.FormattingEnabled = true;
+            this.cbLop.Items.AddRange(new object[] {
+            "SD18301",
+            "SD18302",
+            "SD18303",
+            "SD18304",
+            "SD18305"});
+            this.cbLop.Location = new System.Drawing.Point(227, 337);
+            this.cbLop.Name = "cbLop";
+            this.cbLop.Size = new System.Drawing.Size(387, 28);
+            this.cbLop.TabIndex = 12;
+            this.cbLop.SelectedIndexChanged += new System.EventHandler(this.cbLop_SelectedIndexChanged);
+            // 
+            // txtDiemTB
+            // 
+            this.txtDiemTB.Location = new System.Drawing.Point(227, 399);
+            this.txtDiemTB.Name = "txtDiemTB";
+            this.txtDiemTB.Size = new System.Drawing.Size(387, 27);
+            this.txtDiemTB.TabIndex = 11;
+            this.txtDiemTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDiemTB_KeyPress);
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(227, 274);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(387, 27);
+            this.txtDiaChi.TabIndex = 9;
+            this.txtDiaChi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDiaChi_KeyPress);
+            // 
+            // txtTenHS
+            // 
+            this.txtTenHS.Location = new System.Drawing.Point(227, 141);
+            this.txtTenHS.Name = "txtTenHS";
+            this.txtTenHS.Size = new System.Drawing.Size(387, 27);
+            this.txtTenHS.TabIndex = 7;
+            this.txtTenHS.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTenHS_KeyPress);
+            // 
+            // txtMaHS
+            // 
+            this.txtMaHS.Location = new System.Drawing.Point(227, 74);
+            this.txtMaHS.Name = "txtMaHS";
+            this.txtMaHS.Size = new System.Drawing.Size(387, 27);
+            this.txtMaHS.TabIndex = 6;
+            this.txtMaHS.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMaHS_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(68, 406);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Điểm TB:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(68, 345);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Lớp:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(68, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Địa chỉ: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(68, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ngày sinh:";
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnLuu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Location = new System.Drawing.Point(840, 157);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(115, 44);
+            this.btnLuu.TabIndex = 1;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = false;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(840, 361);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(115, 44);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(840, 474);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(115, 44);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnNhapLai
+            // 
+            this.btnNhapLai.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnNhapLai.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNhapLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhapLai.Location = new System.Drawing.Point(840, 256);
+            this.btnNhapLai.Name = "btnNhapLai";
+            this.btnNhapLai.Size = new System.Drawing.Size(115, 44);
+            this.btnNhapLai.TabIndex = 4;
+            this.btnNhapLai.Text = "Nhập lại";
+            this.btnNhapLai.UseVisualStyleBackColor = false;
+            this.btnNhapLai.Click += new System.EventHandler(this.btnNhapLai_Click);
+            // 
+            // QLHS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 597);
+            this.Controls.Add(this.btnNhapLai);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "QLHS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DANH SÁCH HỌC SINH";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDiemTB;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtTenHS;
+        private System.Windows.Forms.TextBox txtMaHS;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ComboBox cbLop;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.Button btnNhapLai;
+    }
+}
+
---
+++ QLHS.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace hainhph35304_SD18305_Lab3
+{
+    public partial class QLHS : Form
+    {
+        static string connectionString = @"Data Source=Nghh;Initial Catalog=QLHOCSINH;Persist Security Info=True;User ID=sa;Password=nghh206983";
+
+        public QLHS()
+        {
+            InitializeComponent();
+        }
+        public DataSet FillDataSet(string sqlCommand)
+        {
+            DataSet ds = new DataSet();
+            if (string.IsNullOrEmpty(sqlCommand))
+            {
+                return ds;
+            }
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand, conn);
+                    adapter.Fill(ds);
+                    return ds;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.ToString());
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+        public DataTable GetDSLop()
+        {
+            string query = "SELECT MaLop FROM LOP";
+            DataSet ds = FillDataSet(query);
+            return ds.Tables[0];
+        }
+        private void LoadDSLop()
+        {
+            DataTable dt = GetDSLop();
+            cbLop.DataSource = dt;
+            cbLop.DisplayMember = "MaLop";
+        }
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra dữ liệu nhập vào
+            if (string.IsNullOrEmpty(txtMaHS.Text) ||
+                string.IsNullOrEmpty(txtTenHS.Text) ||
+                string.IsNullOrEmpty(dtpNgaySinh.Text) ||
+                string.IsNullOrEmpty(txtDiaChi.Text) ||
+                string.IsNullOrEmpty(cbLop.Text) ||
+                string.IsNullOrEmpty(txtDiemTB.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin học sinh!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Tạo kết nối tới database
+            SqlConnection conn = new SqlConnection("Data Source=Nghh;Initial Catalog=QLHOCSINH;Persist Security Info=True;User ID=sa;Password=nghh206983");
+            conn.Open();
+
+            // Tạo câu truy vấn SELECT để kiểm tra MaHS
+            string checkQuery = "SELECT COUNT(*) FROM HocSinh WHERE MaHS=@MaHS";
+            SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+            checkCmd.Parameters.AddWithValue("@MaHS", txtMaHS.Text);
+
+            // Thực thi câu truy vấn SELECT
+            int count = (int)checkCmd.ExecuteScalar();
+
+            // Kiểm tra nếu MaHS đã tồn tại trong cơ sở dữ liệu
+            if (count > 0)
+            {
+                MessageBox.Show("Mã học sinh đã tồn tại trong cơ sở dữ liệu. Vui lòng nhập lại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMaHS.Focus();
+                return;
+            }
+            // Tạo câu truy vấn INSERT
+            string query = "INSERT INTO HocSinh (MaHS ,TenHS, NgaySinh, DiaChi, MaLop, DTB) VALUES (@MaHS ,@TenHS, @NgaySinh, @DiaChi, @MaLop, @DTB)";
+            SqlCommand cmd = new SqlCommand(query, conn);                   //tạo một đối tượng SqlCommand để thực hiện câu truy vấn SQL
+            cmd.Parameters.AddWithValue("@MaHS", txtMaHS.Text);             //thiết lập tham số @MaHS trong câu truy vấn SQL bằng txtMaHS.Text
+            cmd.Parameters.AddWithValue("@TenHS", txtTenHS.Text);
+            cmd.Parameters.AddWithValue("@NgaySinh", dtpNgaySinh.Value);
+            cmd.Parameters.AddWithValue("@DiaChi", txtDiaChi.Text);
+            cmd.Parameters.AddWithValue("@MaLop", cbLop.Text);
+            cmd.Parameters.AddWithValue("@DTB", txtDiemTB.Text);
+            // Thực thi câu truy vấn INSERT
+            cmd.ExecuteNonQuery();
+
+            // Đóng kết nối
+            conn.Close();
+
+            // Hiển thị thông báo thành công
+            MessageBox.Show("Thêm học sinh thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void txtMaHS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Kiểm tra nếu phím được nhấn không phải là chữ, số
+            if (!char.IsLetterOrDigit(e.KeyChar))
+            {
+                // Không cho phép nhập ký tự đó vào ô văn bản
+                e.Handled = true;
+            }
+        }
+
+        private void txtTenHS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Kiểm tra nếu phím được nhấn không phải là chữ hoặc khoảng trắng
+            if (!char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                // Không cho phép nhập ký tự đó vào ô văn bản
+                e.Handled = true;
+            }
+        }
+
+        private void txtDiaChi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Kiểm tra nếu phím được nhấn không phải là số, chữ, khoảng trắng hoặc dấu -
+            if (!char.IsLetterOrDigit(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar) && e.KeyChar != '-')
+            {
+                // Không cho phép nhập ký tự đó vào ô văn bản
+                e.Handled = true;
+            }
+        }
+
+        private void txtDiemTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Kiểm tra nếu phím được nhấn không phải là số hoặc kí tự .
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                // Không cho phép nhập ký tự đó vào ô văn bản
+                e.Handled = true;
+            }
+            else if (e.KeyChar == '.' && txtDiemTB.Text.IndexOf('.') > -1)
+            {
+                // Không cho phép nhập kí tự . nếu đã có một kí tự . được nhập vào trước đó
+                e.Handled = true;
+            }
+        }
+
+        private void dtpNgaySinh_ValueChanged(object sender, EventArgs e)
+        {
+            // Kiểm tra xem giá trị trong ô DateTimePicker có thay đổi hay không
+            if (dtpNgaySinh.Value == DateTime.Today)
+            {
+                // Hiển thị thông báo yêu cầu người dùng thay đổi giá trị
+                MessageBox.Show("Bạn phải chọn một ngày sinh hợp lệ.");
+            }
+        }
+
+        private void cbLop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Kiểm tra xem giá trị trong ô ComboBox có được chọn hay không
+            if (cbLop.SelectedIndex == -1)
+            {
+                // Hiển thị thông báo yêu cầu người dùng phải chọn lớp
+                MessageBox.Show("Bạn phải chọn một lớp.");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            // Lấy giá trị trong ô văn bản txtMaHS
+            string maHS = txtMaHS.Text;
+
+            // Kiểm tra xem người dùng đã nhập mã sinh viên hay chưa
+            if (maHS != "")
+            {
+                // Tạo kết nối tới database
+                SqlConnection conn = new SqlConnection("Data Source=Nghh;Initial Catalog=QLHOCSINH;Persist Security Info=True;User ID=sa;Password=nghh206983");
+
+                // Tạo câu lệnh SQL để xóa thông tin sinh viên khỏi cơ sở dữ liệu
+                string sql = "DELETE FROM HOCSINH WHERE MaHS = @MaHS";
+
+                // Tạo đối tượng SqlCommand để thực thi câu lệnh SQL
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                // Thêm tham số vào câu lệnh SQL
+                cmd.Parameters.AddWithValue("@MaHS", maHS);
+
+                // Mở kết nối đến cơ sở dữ liệu
+                conn.Open();
+
+                // Thực thi câu lệnh SQL
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                // Đóng kết nối đến cơ sở dữ liệu
+                conn.Close();
+
+                // Kiểm tra xem có thông tin sinh viên nào bị xóa khỏi cơ sở dữ liệu hay không
+                if (rowsAffected > 0)
+                {
+                    // Hiển thị thông báo xóa thành công
+                    MessageBox.Show("Đã xóa thông tin sinh viên có mã " + maHS);
+                }
+                else
+                {
+                    // Hiển thị thông báo không tìm thấy sinh viên
+                    MessageBox.Show("Không tìm thấy sinh viên có mã " + maHS);
+                }
+            }
+            else
+            {
+                // Hiển thị thông báo yêu cầu nhập mã sinh viên
+                MessageBox.Show("Bạn phải nhập mã sinh viên để xóa thông tin.");
+            }
+        }
+
+        private void btnNhapLai_Click(object sender, EventArgs e)
+        {
+            // Reset về ban đầu 
+            txtMaHS.Text = "";
+            txtTenHS.Text = "";
+            txtDiaChi.Text = "";
+            dtpNgaySinh.Value = DateTime.Now;
+            txtDiaChi.Text = "";
+            cbLop.SelectedIndex = 0;
+            txtDiemTB.Text = "";
+            txtMaHS.Focus();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            // Hiển thị hộp thoại xác nhận
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát khỏi chương trình?", "Xác nhận thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // Nếu người dùng xác nhận muốn thoát
+            if (result == DialogResult.Yes)
+            {
+                // Đóng form hiện tại
+                this.Close();
+            }
+        }
+    }
+}
